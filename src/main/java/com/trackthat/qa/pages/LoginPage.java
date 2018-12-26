66721b3f98fc1afc6055d68c6f87b4ea060d1b9a
Ankit Sharma <ankitsharma.as135@gmail.com>
2018-12-26 14:39:46 +0530

    xpath creation

--- src/main/java/com/trackthat/qa/pages/LoginPage.java
+++ src/main/java/com/trackthat/qa/pages/LoginPage.java
@@ -1,4 +1,23 @@
 package com.trackthat.qa.pages;
-public class LoginPage {
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import com.trackthat.QA.Base.TestBase;
+public class LoginPage extends TestBase
+{
+//Page factory -OR:
+@FindBy(xpath="//input[@id='exampleInputEmail1 ']")
+WebElement emailid;
+
+@FindBy(xpath="//input[@name='password11']")
+WebElement password;
+
+@FindBy(xpath="//button[@class='btn btn-default']")
+WebElement loginBtn;
+
+
 
 }
+
+
+
